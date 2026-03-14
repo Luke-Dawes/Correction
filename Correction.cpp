64e66a3d724f343f9afe902ef00ca9f924af42d8
Luke Dawes <luke@daweshome.co.uk>
2026-03-14 10:16:24 +0000

    Add project files.

---
+++ Correction.cpp
@@ -0,0 +1,12 @@
+﻿// Correction.cpp : Defines the entry point for the application.
+//
+
+#include "Correction.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
