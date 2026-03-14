f976a44a1466c1865286781540fd4d016b7820ad
Luke Dawes <luke@daweshome.co.uk>
2026-03-14 10:32:04 +0000

    OpenCV test working

--- Correction.cpp
+++ Correction.cpp
@@ -1,12 +1,22 @@
-﻿// Correction.cpp : Defines the entry point for the application.
-//
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
 
-#include "Correction.h"
+int main() {
+    // 1. Create a blank black image (400x200 pixels)
+    cv::Mat image = cv::Mat::zeros(200, 400, CV_8UC3);
 
-using namespace std;
+    // 2. Add some text to the image
+    std::string text = "OpenCV " + std::string(CV_VERSION) + " works!";
+    cv::putText(image, text, cv::Point(50, 100),
+        cv::FONT_HERSHEY_SIMPLEX, 0.8, cv::Scalar(0, 255, 0), 2);
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+    // 3. Show the image in a window
+    cv::imshow("OpenCV Test", image);
+
+    std::cout << "Successfully loaded OpenCV version: " << CV_VERSION << std::endl;
+    std::cout << "Press any key to close the window..." << std::endl;
+
+    // 4. Wait for a key press to close
+    cv::waitKey(0);
+    return 0;
 }
