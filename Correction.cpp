8271994dcfad4a7d34ded4d36607fe95282b8cd0
Luke Dawes <luke@daweshome.co.uk>
2026-03-15 17:31:04 +0000

    openvino may be alive

--- Correction.cpp
+++ Correction.cpp
@@ -1,7 +1,7 @@
-﻿//error [ERROR:0@0.390] global obsensor_uvc_stream_channel.cpp:163 cv::obsensor::getStreamChannelGroup Camera index out of range Failed to capture frame cause by norton
-
+﻿//error [ERROR:0@0.390] global obsensor_uvc_stream_channel.cpp:163 cv::obsensor::getStreamChannelGroup Camera index out of range Failed to capture frame cause by antivirus
 
 #include <opencv2/opencv.hpp>
+#include <openvino/openvino.hpp>
 #include <iostream>
 
 int main() {
@@ -9,6 +9,8 @@ int main() {
     cv::VideoCapture cap(0);
 
     cv::UMat frame;
+
+	ov::Core core;
 
     while (true) {
         cap >> frame;
