0cad89cd0effba7211d396cc046b278b45d36b70
Luke Dawes <luke@daweshome.co.uk>
2026-03-15 17:55:20 +0000

    back to a much simpler model where i just display the camera where i can try to write the code myself

--- Correction.cpp
+++ Correction.cpp
@@ -1,31 +1,25 @@
-﻿//error [ERROR:0@0.390] global obsensor_uvc_stream_channel.cpp:163 cv::obsensor::getStreamChannelGroup Camera index out of range Failed to capture frame cause by antivirus
-
+﻿#include <openvino/openvino.hpp>
 #include <opencv2/opencv.hpp>
-#include <openvino/openvino.hpp>
 #include <iostream>
 
 int main() {
-    
+ 
     cv::VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        std::cerr << "Cannot open camera\n";
+        return -1;
+    }
 
     cv::UMat frame;
-
-	ov::Core core;
-
     while (true) {
         cap >> frame;
-        if (frame.empty()) {
-            std::cerr << "Failed to capture frame" << std::endl;
-            break;
-        }
+        if (frame.empty()) break;
 
-		cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
-
-        cv::imshow("Camera", frame);
-        if (cv::waitKey(30) >= 0) {
-            break;
-		}
+        cv::imshow("test", frame);
+        if (cv::waitKey(1) == 'q') break;
     }
-    cv::waitKey(0);
+
+    cap.release();
+    cv::destroyAllWindows();
     return 0;
 }
